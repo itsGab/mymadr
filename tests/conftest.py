a11ac28acd2579f320fe0c90a06ea85527abddbc
gabriel <gabriel.schultz10@gmail.com>
2025-10-28 18:37:24 -0300

    prepara o codigo para a inclusao do alembic

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from mymadr.app import app
+from mymadr.models import table_registry
+from mymadr.settings import Settings
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine(Settings.DATABASE_URL)
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
+    engine.dispose()
