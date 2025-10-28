f31ceb900ead2d88f441334f4b355426db7f2c1b
gabriel <gabriel.schultz10@gmail.com>
2025-10-28 17:52:57 -0300

    add models das tabelas do banco de dados

---
+++ mymadr/models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
+
+table_registry = registry()
+
+
+@table_registry.mapped_as_dataclass
+class Account:
+    __tablename__ = 'accounts'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+
+
+
+@table_registry.mapped_as_dataclass
+class Book:
+    # LIVRO tem id relationship com ROMANCISTA
+    __tablename__ = 'books'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    title: Mapped[str] = mapped_column(unique=True)
+    # FIXME: colocar um validacao no Ano do livro.
+    year: Mapped[int] = mapped_column()
+    author_id: Mapped[int] = mapped_column(ForeignKey('accounts.id'))
+
+
+
+@table_registry.mapped_as_dataclass
+class Author:
+    __tablename__ = 'authors'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    books: Mapped[list['Book']] = relationship(
+        init=False,
+        cascade='all, delete-orphan',
+        lazy='selectin',
+    )
