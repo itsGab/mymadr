5b7f934c82534923951d86881f05dbe79ed7c432
gabriel <gabriel.schultz10@gmail.com>
2025-11-05 00:04:54 -0300

    faz listagem de testes como guia

---
+++ tests/test_books.py
@@ -0,0 +1,17 @@
+
+""" LISTANDO TESTES:
+register book success
+register book when novelist does not exist not found
+get book by id success
+get book by id when book does not exist not found
+query book by partial title success
+query book by year success
+query book by novelist_id success
+query book by no matches return empty list
+update book title success
+update book year success
+update book novelist_id success ??? does it makes sense since is relational ???
+update book with existing title success ??? make it unique ???
+delete book success
+delete book not authenticated unauthorized
+"""
---
+++ tests/test_novelists.py
@@ -0,0 +1,17 @@
+
+""" LISTANDO TESTES:
+register novelist success
+register novelist with existing novelist conflict
+get novelist by id success
+get novelist by id when novelist does not exist not found
+query novelist by partial name success
+query novelists by partial name success
+query novelists no matches returns empty list
+query novelists with more than 20 novelists pagination success
+update novelist sucess
+update novelist when novelist does not exist not found
+update novelist with existing novelist name conflict
+delete novelist success
+delete novelist with other novelist id unauthorized
+delete novelist not authenticated unauthorized
+"""
